ad1228cce2feb640cc54b582dbb3f7ea5ec6d181
minhosong88 <hominsong@naver.com>
2024-08-28 16:19:54 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [loading, SetLoading] = useState(true);
+  const [coins, setCoins] = useState([])
+  
+  const coinSymbols = ["BTC", "ETH", "BNB", "DOGE", "USDT"];
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((data) => {
+        const filteredCoins = data.filter((coin) => coinSymbols.includes(coin.symbol));
+        setCoins(filteredCoins);
+        SetLoading(false);
+      });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Coin Tracker {loading ? "": `(${coins.length})`}</h1>
+      {loading && <strong>Loading...</strong>}
+      <select hidden={loading === true}>
+        {coins.map((coin) => (
+          <option key={coin.id}>{coin.name}({coin.symbol}): {coin.quotes.USD.price} USD</option>
+        ))}
+      </select>
     </div>
   );
 }
